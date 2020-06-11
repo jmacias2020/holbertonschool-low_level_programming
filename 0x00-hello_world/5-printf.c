57861ae447c2528c61a7a347d48b6caa69b42234
jmacias2020 <jmaciasvesga@hotmail.com>
2020-06-11 01:10:00 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
 #!/bin/bash
-printf("with proper grammar, but the outcome is a piece of art,\n")
+
+#include <stdio.h>
+
+main()
+{
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+
+}
