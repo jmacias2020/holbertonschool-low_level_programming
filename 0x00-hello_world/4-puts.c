5fa33af05943a9b4d0d1933ee613103c1aa16baf
jmacias2020 <jmaciasvesga@hotmail.com>
2020-06-11 04:11:15 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - use put no allow use printf
+ * Description: uses puts to print message
+ * Return: 0
+ */
 int main(void)
-
-
 {
-
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-
 }
