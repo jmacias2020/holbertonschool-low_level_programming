0682e60c22323e0e1e28229207f4dd7c66945cac
jmacias2020 <jmaciasvesga@hotmail.com>
2020-06-11 04:17:47 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - use printf, no allow use puts
+ * Description: uses printf to print message and new line
+ * Return: 0
+ */
 int main(void)
-
-
 {
-
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-
 }
