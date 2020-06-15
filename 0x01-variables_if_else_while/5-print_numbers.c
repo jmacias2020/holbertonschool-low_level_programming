f955c6a7aa0f91585d5a009fa81bf06f8837cfdf
jmacias2020 <jmaciasvesga@hotmail.com>
2020-06-15 04:17:19 +0000

    solucion 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - assign the alphabet.
+ *
+ * Return: 0 indicates sucesfull execution.
+ */
+
+int main(void)
+{
+
+int a = 0;
+
+while (a <= 9)
+{
+
+printf("%d", a);
+
+a = a + 1;
+
+}
+putchar('\n');
+return (0);
+}
